ff7d695030a432bc916e40b3f1f3a23881254f17
Nuwantha Fernando <wnsfernando95@gmail.com>
2020-07-30 08:57:40 +0530

    Spring boot project initialized

---
+++ twilio_client_spring/src/main/java/com/example/twilio_client_spring/TwilioClientSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.twilio_client_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioClientSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioClientSpringApplication.class, args);
+	}
+
+}
---
+++ twilio_client_spring/src/test/java/com/example/twilio_client_spring/TwilioClientSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twilio_client_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioClientSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
